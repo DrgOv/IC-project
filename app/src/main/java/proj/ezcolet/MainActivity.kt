66d27e841a982c2b071b5da567c395316f9149c4
DrgOv <ovidiu.dragoi00@gmail.com>
2021-03-03 21:05:15 +0200

    changed login background

--- app/src/main/java/proj/ezcolet/MainActivity.kt
+++ app/src/main/java/proj/ezcolet/MainActivity.kt
@@ -1,15 +1,17 @@
-package proj.ezcolet.controllers
+package proj.ezcolet
 
 import android.content.Intent
+import android.graphics.PixelFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import proj.ezcolet.databinding.ActivityMainBinding
 import proj.ezcolet.services.ViewService
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        window.setFormat(PixelFormat.RGBA_8888)
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
