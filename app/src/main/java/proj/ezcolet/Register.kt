050f9d94e9eb7e25bef1c177b431b45117ccbe79
nbat9 <nbat99@yahoo.com>
2021-03-05 23:26:43 +0200

    Created the registration page

---
+++ app/src/main/java/proj/ezcolet/Register.kt
@@ -0,0 +1,14 @@
+package proj.ezcolet
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class Register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
