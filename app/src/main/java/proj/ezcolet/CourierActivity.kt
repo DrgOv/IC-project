c7178af07429c7916f12e0f0e82577b3a5440969
nbat9 <nbat99@yahoo.com>
2021-03-10 20:35:33 +0200

    Courier's info page and some fixes

--- app/src/main/java/proj/ezcolet/CourierActivity.kt
+++ app/src/main/java/proj/ezcolet/CourierActivity.kt
@@ -1,6 +1,8 @@
 package proj.ezcolet
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_courier.*
@@ -10,6 +12,13 @@ class CourierActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_courier)
+
+        val button:ImageButton=findViewById(R.id.infoImageBtn)
+        button.setOnClickListener{
+            val intent= Intent(this,CourierInfoActivity::class.java)
+            startActivity(intent)
+        }
+
         val list = generateList(500)
         recyclerView.adapter = Adapter(list)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -20,7 +29,7 @@ class CourierActivity : AppCompatActivity() {
         val list = ArrayList<ExampleItem>()
         for (i in 0 until size) {
 
-            val item = ExampleItem("Comanda $i", "livrat la ora:")
+            val item = ExampleItem("Comanda $i", "  livrat la ora:")
             list += item
         }
         return list
