cb699191a375c0f5568607b74e7ca023fba6b273
DrgOv <ovidiu.dragoi00@gmail.com>
2021-03-10 22:42:12 +0200

    added rush delivery icon

--- app/src/main/java/proj/ezcolet/UserInfoActivity.kt
+++ app/src/main/java/proj/ezcolet/UserInfoActivity.kt
@@ -2,6 +2,7 @@ package proj.ezcolet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import proj.ezcolet.R
 
 class UserInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
