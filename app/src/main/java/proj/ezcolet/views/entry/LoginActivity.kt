dcc482938e827654d705d49f867bf80074ef0456
DrgOv <ovidiu.dragoi00@gmail.com>
2021-03-14 13:40:25 +0200

    added firebase (firestore)

--- app/src/main/java/proj/ezcolet/views/entry/LoginActivity.kt
+++ app/src/main/java/proj/ezcolet/views/entry/LoginActivity.kt
@@ -3,7 +3,10 @@ package proj.ezcolet.views.entry
 import android.graphics.PixelFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 import proj.ezcolet.databinding.EntryLoginActivityBinding
 import proj.ezcolet.services.ViewService
 import proj.ezcolet.views.client.ClientActivity
@@ -17,6 +20,20 @@ class LoginActivity : AppCompatActivity() {
         window.setFormat(PixelFormat.RGBA_8888)
 
         val binding = EntryLoginActivityBinding.inflate(layoutInflater)
+
+        binding.loginBtn.setOnClickListener() {
+            val city = hashMapOf(
+                "name" to "Los Angeles",
+                "state" to "CA",
+                "country" to "USA"
+            )
+
+            val db = FirebaseFirestore.getInstance()
+            db.collection("clients").document("LA")
+                .set(city)
+                .addOnSuccessListener {}
+                .addOnFailureListener { }
+        }
 
         binding.registerBtn.setOnClickListener() {
             ViewService.setView(this, RegisterActivity())
