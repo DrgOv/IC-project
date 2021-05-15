8a26c18dbe7209c34618245f748afb096c0b60c8
bogdancolhon <nbat99@yahoo.com>
2021-05-15 12:39:36 +0300

    Client can see just the orders from the current day

--- app/src/main/java/proj/ezcolet/presenters/client/ClientHomePresenter.kt
+++ app/src/main/java/proj/ezcolet/presenters/client/ClientHomePresenter.kt
@@ -11,6 +11,7 @@ import proj.ezcolet.services.database.FsClientService
 import proj.ezcolet.services.database.FsCourierService
 import proj.ezcolet.services.database.FsQueryingService
 import proj.ezcolet.views.client.ClientHomeActivity
+import java.util.*
 
 class ClientHomePresenter(private val clientHomeActivity: ClientHomeActivity) {
     private lateinit var client: ClientModel
@@ -20,10 +21,11 @@ class ClientHomePresenter(private val clientHomeActivity: ClientHomeActivity) {
     }
 
     private suspend fun getOptions(): FirestoreRecyclerOptions<OrderModel> {
+        val currentDay = getDate()
         val order = client.getOrdersByUsername()[0]
         return ViewService.setFsRecyclerAdapterOptions(
-            FsQueryingService.getOrdersQueryWhereEqualsTo(
-                "courierUsername", order.courierUsername
+            FsQueryingService.getOrdersQueryWhereEqualsToDay(
+                "courierUsername", order.courierUsername,"orderDate",currentDay
             )
         )
     }
@@ -37,5 +39,21 @@ class ClientHomePresenter(private val clientHomeActivity: ClientHomeActivity) {
         clientHomeActivity.setRemainingOrdersNumberText("0");
         val courier = FsCourierService.getCourier(client.getOrdersByUsername()[0].courierUsername)!!
         clientHomeActivity.setCourierUsernameText("Curier: ${courier.firstName} ${courier.lastName}")
+    }
+
+     private fun getDate(): String {
+        val calendar: Calendar = Calendar.getInstance()
+        val month: String =
+            calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale("ro"))
+
+        val dayOfMonth: Int = calendar.get(Calendar.DAY_OF_MONTH)
+        val dayOfMonthStr = dayOfMonth.toString()
+
+        val year: Int = calendar.get(Calendar.YEAR)
+        val yearStr = year.toString()
+
+        val date = "$dayOfMonthStr $month $yearStr"
+        return date
+
     }
 }
